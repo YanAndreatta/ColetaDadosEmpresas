bd95b87c666053fe6b1f957f7d24f039583019b4
Yan Andreatta <yan.duca2@gmail.com>
2023-08-14 14:16:21 -0300

    teste .gitignore

---
+++ key.py
@@ -0,0 +1,3 @@
+def key_pass():
+    key_pass = 'AIzaSyDYwZr4ujGKFkExArZyto-t1CzLItjZ41Q'
+    return key_pass
---
+++ location.py
@@ -0,0 +1,7 @@
+def location_latitude():
+    latitude =  -30.154639805554698
+    return location_latitude
+
+def location_longitude():
+    longitude = -51.16838741644834
+    return location_longitude
