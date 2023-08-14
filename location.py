9cdb801966e5b3cf51b79f9f74e6bc0f64073aab
Yan Andreatta <yan.duca2@gmail.com>
2023-08-14 14:19:53 -0300

    start ignoring location.py

--- location.py
+++
@@ -1,7 +0,0 @@
-def location_latitude():
-    latitude =  -30.154639805554698
-    return location_latitude
-
-def location_longitude():
-    longitude = -51.16838741644834
-    return location_longitude
